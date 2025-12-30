8fcfadcba7a644eca52c79924566088143710520
Punit Subudhi <punitsubudhi@gmail.com>
2025-12-30 16:58:44 +0530

    Add configuration loader using python-dotenv

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_USER = os.getenv("DB_USER", "user")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
+    DB_NAME = os.getenv("DB_NAME", "mydatabase")
+    
+__all__ = ["Config"]
